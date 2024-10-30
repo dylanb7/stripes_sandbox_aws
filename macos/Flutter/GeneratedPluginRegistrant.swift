99c8140055e5cc84f77287b4b2b88f7c05c49866
Dylan Baker <dylanbaker7@gmail.com>
2024-10-30 15:09:25 -0400

    f

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,8 +12,6 @@ import device_info_plus
 import flutter_secure_storage_macos
 import package_info_plus
 import path_provider_foundation
-import share_plus
-import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
 
@@ -25,8 +23,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -6,7 +6,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   amplify_db_common
   connectivity_plus
   flutter_secure_storage_windows
-  share_plus
   url_launcher_windows
 )
 
